cce60e9d55b7a871779de636fa2b2fb0dc221d0f
Garvit <garvit.joshi@byjus.com>
2023-07-21 15:07:30 +0530

    initial comit added the entire react project

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Wrapper from "./layout/wrapper";
+import Home10 from "./home/home_10";
+import Home9 from "./home/home_9";
+import Home8 from "./home/home_8";
+import Home7 from "./home/home_7";
+import Home6 from "./home/home_6";
+import Home5 from "./home/home_5";
+import Home4 from "./home/home_4";
+import Home3 from "./home/home_3";
+import Home2 from "./home/home_2";
+import Home1 from "./home/home_1";
+
+const MainRoot = () => {
+  return (
+    <Wrapper>
+      <Home10 />
+    </Wrapper>
+  );
+};
+
+export default MainRoot;
